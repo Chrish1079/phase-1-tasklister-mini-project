aac504da0b8bd8a568b0c13d35121fb1c1127b03
Chrish1079 <itssuper1079@gmail.com>
2025-10-22 19:57:40 -0400

    submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+
+    const input = e.target.querySelector("#new-task-description").value;
+
+    const ul = document.getElementById("tasks");
+    const li = document.createElement("li");
+    li.textContent = input;
+    ul.appendChild(li);
+
+    e.target.reset();
+  });
 });
+
+  // your code here
+
